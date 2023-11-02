188fcc6e32507e717feca13a6b512536ccb3f1eb
Shabero <tokmokiuca@gmail.com>
2023-11-02 20:00:12 +0600

    HomeWork done!

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-// const arr = [1,2,3,4,5]
-// const handleFilter = (arg) => {
-//     return arg.reduce((acc,el) => acc += el, 0)
+// Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for найдите сумму элементов этого массива. Запишите ее в переменную result.
+
+// let arr = [1, 2, 3, 4, 5];
+// let result = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     result += arr[i];
 // }
-//
-// console.log(handleFilter(arr))
+// console.log(result);
 
-// const names = ['John', 'Nikolas', 'Evdokim', 'Aidin']
-// const  handleFunction = (arg) => {
-//     return arg.reduce(( acc,el,idx ) => {
-//         return [...acc,{name: el,id: idx+1 }]
-//     },[])
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] => сумму всех элементов массива
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let result = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     result += arr[i];
 // }
+// console.log(result);
+
+// Дан массив с элементами [1, 2, 5, 9, 4, 13, 4, 10].
+// С помощью цикла for и оператора if проверьте есть ли в массиве элемент со значением, равным 4.
+// Если есть - выведите на экран 'Есть!' и выйдите из цикла. Если нет - 'Нет!'
+// let arr = [1, 2, 5, 9, 4, 13, 4, 10];
+// let found = false;
 //
-// console.log(handleFunction(names))
-
-function likes(names) {
-    if (names.length < 1 ) {
-        return 'no one likes this'
-    } else if (names.length < 3 ) {
-        return  `${names[0]} and ${names[1]} like this`
-    } else if (names.length < 5) {
-        return `${names[0]} and ${names[1]} and ${names[2]} like this`
-    }
-    return names
-    // } else if (names.length < 7) {
-    //      return `${names[0]} , ${names[1]} , ${names[2]} and ${names[3]} like this`
-    // }
-}
-
-console.log(likes([])) // no one likes this
-console.log(likes(['Jacob','Alex']))  //Jacob and Alex like this
-console.log(likes('Peter', 'BEka', 'Shabdan')) // Jacob,Alex and Shabdan like this
-console.log(likes('Maksat', 'FEra', 'Shaba', 'Farukh')) // Jacob, Alex, Shabdan and Farukh like this
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 4) {
+//         found = true;
+//         break;
+//     }
+// }
+// if (found) {
+//     console.log('Есть!');
+// } else {
+//     console.log('Нет!');
+// }
